3b586c9acb3c412a1d7abbdd9c57537c1013c67c
Michael Mondoro <michaelmondoro@Michaels-MacBook-Air.local>
2022-03-14 12:28:01 -0400

    added API file

---
+++ miracles.py
@@ -0,0 +1,83 @@
+from flask import Flask, Response, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+def getCountryMiracles(miracles):
+    data = []
+    for miracle in miracles:
+        data.append({   'url':miracle[0], 
+                        'date':miracle[1],
+                        'country':miracle[2],
+                        'city':miracle[3]})
+    return data
+
+def getSaintMiracles(miracles):
+    data = []
+    for miracle in miracles:
+        data.append({   'url':miracle[0], 
+                        'saint':miracle[1],
+                        'date':miracle[2]})
+    return data
+
+def getMarianMiracles(miracles):
+    data = []
+    for miracle in miracles:
+        data.append({   'url':miracle[0], 
+                        'date':miracle[1],
+                        'country':miracle[2],
+                        'city':miracle[3],
+                        'name':miracle[4]})
+    return data
+
+def getCommunionMiracles(miracles):
+    data = []
+    for miracle in miracles:
+        data.append({   'url':miracle[0], 
+                        'name':miracle[1]})
+    return data
+
+
+@app.route('/', methods=["GET"])
+def index():
+    return "WELCOME"
+
+@app.route('/countries', methods=["GET"])
+def countries():
+    con = sqlite3.connect('miracles.db')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM countryMiracles')
+    miracles = cur.fetchall()
+    data = getCountryMiracles(miracles)
+    return jsonify(data), 201
+
+@app.route('/saints', methods=["GET"])
+def saints():
+    con = sqlite3.connect('miracles.db')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM saintMiracles')
+    miracles = cur.fetchall()
+    data = getSaintMiracles(miracles)
+    return jsonify(data), 201
+
+@app.route('/marian', methods=["GET"])
+def marian():
+    con = sqlite3.connect('miracles.db')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM marianMiracles')
+    miracles = cur.fetchall()
+    data = getMarianMiracles(miracles)
+    return jsonify(data), 201
+
+
+@app.route('/communions', methods=["GET"])
+def communions():
+    con = sqlite3.connect('miracles.db')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM communionMiracles')
+    miracles = cur.fetchall()
+    data = getCommunionMiracles(miracles)
+    return jsonify(data), 201
+
+if __name__=='__main__':
+    app.run(debug=True)
